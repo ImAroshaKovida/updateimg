4d113f06999b937af45229af849091f94035ace3
ImArosha <92624236+ImAroshaKovida@users.noreply.github.com>
2022-03-20 14:12:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,42 @@
 class script(object):
 
-    START_MSG = """ <b>Hi {}
+    START_MSG = """ 
+╔┓┏╦━━╦┓╔┓╔━━╗ 
+║┗┛║┗━╣┃║┃║╯╰║ 
+║┏┓║┏━╣┗╣┗╣╰╯║ 
+╚┛┗╩━━╩━╩━╩━━~~~
+I'm Baymax
+I'm a Simple Yet Powerful bot With Cool Modules. Made by Arosha_Kovida(t.me/Aro_Ediz)
+Hit HELP button to find my list of available commands
+    
+ 
+ᴀʀᴏ|ᴇᴅɪᴢ • ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ"""
+    
+    HELP_MSG = """Heya, available commands here!
+    
+<code>⚙ /aro </code>[ For download songs ]
 
-I'm a Image Editor Bot which Supports various modes
+      Ex: /aro faded 
+      
+Follow these steps to edit your photos!      
 
-For more click help....</b>"""
-    HELP_MSG = """Hai, Follow these Steps..
+<code>⚙ Send me any Image to Edit..</code>
 
-<code>🌀 Send me any Image to Edit..</code>
+<code>⚙ Select the Corresponding mode that you need</code>
 
-<code>🌀 Select the Corresponding mode that you need</code>
+<code>⚙ Your Edited Image will be Uploaded </code>
 
-<code>🌀 Your Edited Image will be Uploaded </code>
+Enjoy!!!😉👌
 
 
-© @TroJanzHEX"""
-    ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
+
+ᴀʀᴏ|ᴇᴅɪᴢ • ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ"""
+    
+    
+    ABOUT_MSG = """⭕️<b>My Name : The Baymax</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+⭕️<b>Owner :</b> <a href='t.me/Aro_Ediz'>Arosha_Kovida</a>
 
 ⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
